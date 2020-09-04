79605e1af07d93c73e27975016d3e724a8a900ba
fernandaveiga <70073518+fernandaveiga@users.noreply.github.com>
2020-09-04 18:16:07 -0300

    criei uma função de volume de paralelepipedo

---
+++ exemplo/exemplo_principal.py
@@ -0,0 +1,12 @@
+def vParalelepipedo(l, a, p):
+    '''
+    calculo do volume de um paralelepipedo
+    l=largura
+    a=altura
+    p=profundidade
+    '''
+
+    volume = l*a*p
+    return volume
+
+print(vParalelepipedo(2,2,2))
