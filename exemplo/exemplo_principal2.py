9c96c0e9996c6fb3ca8bd28cd1521623823285c8
fernandaveiga <70073518+fernandaveiga@users.noreply.github.com>
2020-09-05 08:19:14 -0300

    Update exemplo 2
    Update para que os usuários digitem as dimensoes do paralelepípedo.

--- exemplo/exemplo_principal2.py
+++ exemplo/exemplo_principal2.py
@@ -1,7 +1,10 @@
 from exemplo_principal import vParalelepipedo, atParalelepipedo
 
-v = vParalelepipedo(3,3,3)
-areat = atParalelepipedo(3,3,3)
+l = int(input('Digite a largura do paralelepípedo: '))
+a = int(input('Digite a altura do paralelepípedo: '))
+p = int(input('Digite a pofundidade do paralelepípedo: '))
+v = vParalelepipedo(l,a,p)
+areat = atParalelepipedo(l,a,p)
 print(f'O volume do paralelepípedo é igual a {v} e a área total é igual a {areat}')
 
 
